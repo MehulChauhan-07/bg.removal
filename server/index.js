a829e15c47bdb02eaf527068f14427718603009c
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-07-01 20:40:06 +0530

    feat: Add initial server setup with Express, MongoDB connection, and webhook handler

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// /api/index.js
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+app.use(express.json());
+
+import cors from "cors";
+import connectDB from "./config/mongodb.js";
+
+await connectDB();
+// Sample route
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from Express on Vercel!" });
+});
+
+module.exports.handler = serverless(app);
