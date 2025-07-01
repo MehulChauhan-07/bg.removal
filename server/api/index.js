cffe3bd7b1d6797f98232b81f1eb07624d173c35
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-07-01 21:10:44 +0530

    fix: Simplify server setup by removing unused dependencies and redundant code

--- server/api/index.js
+++ server/api/index.js
@@ -1,31 +1,8 @@
-// api/index.js
 const express = require("express");
-const serverless = require("serverless-http");
-const cors = require("cors");
-const connectDB = require("../config/mongodb");
-const dotenv = require("dotenv");
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-// connect to DB inside an async wrapper
-connectDB()
-  .then(() => {
-    console.log("✅ MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection failed:", err);
-  });
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Sample route
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express on Vercel!" });
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// module.exports.handler = serverless(app);
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
-});
-
-module.exports = app; // Export the app for serverless deployment
-module.exports.handler = serverless(app); // Export the handler for serverless deployment
+module.exports = app;
