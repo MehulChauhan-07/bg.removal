c6fdd7b0b8c9161cd60bc71911e3a9c73ecd6724
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-07-01 22:54:46 +0530

    fix: Comment out MongoDB connection code in index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
-const connectDB = require("../config/mongodb.js");
+// const connectDB = require("../config/mongodb.js");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-connectDB()
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+// connectDB()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
 
 // Sample route
 app.get("/", (req, res) => {
